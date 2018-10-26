54512572d8bdb730876116be83c19310fa482187
Max Starikevich <maxim.starikevich@gmail.com>
2018-10-26 11:38:14 +0300

    Some basic src setup

---
+++ src/client/index.jsx
@@ -0,0 +1,10 @@
+'use strict'
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Index = () => {
+  return <div>Game of Life</div>
+}
+
+ReactDOM.render(<Index />, document.getElementById('app'))
