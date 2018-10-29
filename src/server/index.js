c0a5ec2a74b071f0511a125c98d5a20eba86b436
Max Starikevich <maxim.starikevich@gmail.com>
2018-10-29 13:53:43 +0300

    Started implementing server functionality

--- src/server/index.js
+++ src/server/index.js
@@ -1,1 +1,15 @@
 'use strict'
+
+const fs = require('fs')
+const path = require('path')
+
+const Koa = require('koa')
+const app = new Koa()
+const server = require('http').createServer(app.callback())
+
+server.listen(3000, '0.0.0.0')
+
+app.use(async (ctx) => {
+  ctx.type = 'html'
+  ctx.body = fs.createReadStream(path.resolve(process.pwd + '/../dist/index.html'))
+})
