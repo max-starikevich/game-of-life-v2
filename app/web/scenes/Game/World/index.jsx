576d903ceab637022cf6aca93afc0ff85396dec7
Max Starikevich <maxim.starikevich@gmail.com>
2018-10-30 12:12:03 +0300

    Added World react component

---
+++ app/web/scenes/Game/World/index.jsx
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class World extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div className="world">
+        World component
+      </div>
+    )
+  }
+}
+
+export {
+  World
+}
+
