de3fb1878a91004138d0471d2098ea3477360674
Max Starikevich <maxim.starikevich@gmail.com>
2018-11-05 15:05:33 +0300

    Refactoring in webpack's production config

--- src/client/webpack/production.config.js
+++ src/client/webpack/production.config.js
@@ -1,6 +1,5 @@
-const path = require('path')
 const merge = require('webpack-merge')
-const {contextPath, baseConfig} = require('./base.config')
+const {baseConfig} = require('./base.config')
 
 module.exports = merge(baseConfig, {
   mode: 'production'
